0766ebf21621ff25bcb1b68cc85526f330df16a0
Durantay Quentin <quentin.durantay@gmail.com>
2017-03-21 22:18:37 +0100

    Finish challenge for first day \o/

--- Day 1 - JS Drum Kit/app.js
+++ Day 1 - JS Drum Kit/app.js
@@ -1,8 +1,22 @@
+function toggleKey(e) {
+    const key = document.querySelector(`div[data-key="${e.which}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.which}"]`);
+
+    if (!audio) {
+        return null;
+    }
+
+    key.classList.add('playing');
+    audio.play();
+    key.addEventListener('transitionend', removeStyling);
+}
+
+function removeStyling(e) {
+    this.classList.remove('playing');
+}
+
 const keys = document.getElementsByClassName('keys')[0].children;
 console.log(keys);
 
-keys[0].onClick(addAnimation(keys[0]));
+keys.forEach(addEventListener('keydown', toggleKey));
 
-function addAnimation(key) {
-    key.classList.toggle('playing');
-}
